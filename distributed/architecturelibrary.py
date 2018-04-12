7b7597da3f843a40cef0c11cbfb7041ed8686b17
Laurent Evain <laurent.evain@univ-angers.fr>
2018-04-13 01:42:26 +0200

    Room added and checked, undocumented yet

--- distributed/architecturelibrary.py
+++ distributed/architecturelibrary.py
@@ -13,8 +13,9 @@ from lights import *
 
 class Wall(Prism):
     """ Class for walls, not necessarily rectangular or vertical, but the base is a tetragon """
-    def __new__(cls,polyline,verticalVector,thickness=0,name=""):
+    def __new__(cls,polyline,verticalVector=Z,thickness=0,name=""):
         # items 0 and 1 in the polyline are respectivly the left and right points on the floor of the interior wall
+        print(polyline)
         self=Prism.from_polyline_vector(polyline,verticalVector)
         return self
     def __init__(self,polyline,verticalVector,thickness=0,name=""):
@@ -22,9 +23,11 @@ class Wall(Prism):
             self.thickness=thickness
         if not name:
             self.name=name
-        self.armature=Segment(0.5*(polyline[0]+polyline[-1]),0.5*(polyline[2]+polyline[3])) # aka the line on the floor in the middle of the wall
+        self.armature=Segment(0.5*(polyline[0]+polyline[3]),0.5*(polyline[1]+polyline[2])) # aka the line on the floor in the middle of the wall
         self.outsideBaseLine=Segment(polyline[2],polyline[3]) # the intersection of the floor and the exterior wall. The points are the extremal points of this line
-        self.insideBaseLine=Segment(polyline[0],polyline[1]) 
+        self.insideBaseLine=Segment(polyline[0],polyline[1])
+        print(self.outsideBaseLine,"obl")
+        print(self.insideBaseLine)
         self.verticalVector=verticalVector # alread accessible by self.prismDirection but more readable alias in the context of walls
         insideVector=self.outsideBaseLine.vector.cross(verticalVector)
         if (self.insideBaseLine[0]-self.outsideBaseLine[0]).dot(insideVector) < 0:
@@ -49,48 +52,50 @@ class Wall(Prism):
 
 
     
-# class Room(Compound):
-#     """ 
-#     The room is suppose to have right angle corners so that its walls are cubes. 
-#     """
-#     def __init__(self,floor=Polygon(origin-X-Y,2*Y,2*X,-2*Y,-2*X),wallThickness=0.2,height=2):
-#         """
-#         This constructs a room whose exterior walls are along the floor. The interior walls are computed using 
-#         wall thickness. The polygon for the floor is in the plane z=0, with the points listed clockwise.
-#         A floor and a ceiling are added. 
-#         Remark: if the point are listed counterclockwise, this messes the inside and outside of the room. 
-#         Doable in the future: compute the index of the polygon to know if it is clockwise. 
-#         """
-#         walls=[]
-#         for segment,segment.index in polyline.segments():
-#             wall=Object()
-#             armature=segment
-#             insideVector=segment.normalized_copy().rotate(-Z,math.pi/2)
-#             insideWall=segment.copy().translate(insideVector*wallThickness*.5)
-#             outsideWall=segment.copy().translate(-insideVector*wallThickness*.5)
-#             name="wall"+str(segment.index)
-#             #walls.append(Wall(polyline,vector,armature,insideVector,outsideBaseLine,insideBaseLine)):
-#         intPoints=[]
-#         extPoints=[]
-#         for wall,wall.index in walls:
-#             previousWall=walls[wall.index-1]
-#             wall.insideLeftPoint=point.from_2_lines(wall.insideWall,previousWall.insideWall)
-#             previousWall.insideRightPoint=wall.insideLeftPoint
-#             intPoints.append(wall.insideLeftPoint)
-#             wall.outsideLeftPoint=point.from_2_lines(wall.outsideWall,previousWall.outsideWall)
-#             previousWall.outsideRightPoint=wall.outsideLeftPoint
-#             intPoints.append(wall.outsideLeftPoint)
-#             walls.append(Wall(wall))
-#         floor=polygon(intPoints)
-#         ceiling=floor.copy.translate(height*Z)
-#         Compound.__init__(self,[["walls",walls],["ceiling",ceiling],["floor",floor]])
-
-
+class Room(Compound):
+    """ 
+    A class to build a 3d-room from a 2D plan
+    """
+    def __init__(self,floor=Polyline([origin-X-Y,2*Y,2*X,-2*Y,-2*X]),wallThickness=0.2,height=2,verticalVector=Z):
+        """
+        This constructs a room whose exterior walls are along the floor. The interior walls are computed using 
+        wall thickness. The polygon for the floor is in the plane z=0, with the points listed clockwise.
+        A floor and a ceiling are added. 
+        Remark: if the point are listed counterclockwise, this messes the inside and outside of the room. 
+        Doable in the future: compute the index of the polygon to know if it is clockwise. 
+        """
+        logicalWalls=[]
+        outPoints=[]
+        for index,segment in enumerate(floor.segments()):
+            lw=Object()
+            #lw.armature=segment
+            lw.insideVector=segment.vector.normalized_copy().rotate(-verticalVector,math.pi/2)
+            lw.insideWall=segment.copy().translate(lw.insideVector*wallThickness*.5)
+            lw.outsideWall=segment.copy().translate(-lw.insideVector*wallThickness*.5)
+            lw.name="wall"+str(index)
+            logicalWalls.append(lw)
+        for windex,logicalWall in enumerate(logicalWalls):
+            previousWall=logicalWalls[windex-1]
+            logicalWall.insideLeftPoint=Point.from_2_lines(logicalWall.insideWall,previousWall.insideWall)
+            previousWall.insideRightPoint=logicalWall.insideLeftPoint
+            logicalWall.outsideRightPoint=Point.from_2_lines(logicalWall.outsideWall,previousWall.outsideWall)
+            previousWall.outsideLeftPoint=logicalWall.outsideRightPoint
+            outPoints.append(logicalWall.outsideRightPoint)
+        walls=[]
+        for lw in logicalWalls:
+            polyline=Polyline([lw.insideLeftPoint,lw.insideRightPoint,lw.outsideLeftPoint,lw.outsideRightPoint,lw.insideLeftPoint])
+            walls.append(Wall(polyline,height*verticalVector,wallThickness))
+        floor=Polygon(outPoints).translate(0.000000001*verticalVector) # translation so that it is above the floor
+        ceiling=floor.copy().translate(height*verticalVector)
+        liste=[["wall"+str(i),wall] for i,wall in enumerate(walls)]
+        Compound.__init__(self,liste+[["ceiling",ceiling],["floor",floor]])
+        self.walls=walls
+        #Compound.__init__(self,liste)
+        #Compound.__init__(self,liste+[floor])
+        #Compound.__init__(self,[floor,ceiling])
 
 """    
-* faire des wall qui heritent de prisme
 * ajouter fenetres et portes,
 * ajouter des textures
-* Eventuellement faire avec des murs fins.
-* ajouter le prisme et le polygone a la doc
+* ajouter le prisme et le polygone,le wall, la Room a la doc
 """
